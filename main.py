02bce39bce5e4f9f899ba61f6544a5506671cc3f
qunshiyan1 <2420980652@qq.com>
2025-09-09 17:30:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-# 在这个文件下编写代码，题目具体要求见README.md文件
+weight = input("请输入带有字符的重量")
+if weight[-2:] in ['kg']:
+  qianke = (eval (weight[0:-2]))/2.2046
+  print("对应的公制重量为{:.3f}公斤".format(qianke))
+elif weight[-2:] in ['pd']:
+  bang = (eval (weight[0:-2]))*2.2046
+  print("对应的英镑重量为{:.3f}磅".format(bang))
+else:
+  print("输入格式错误")
