a845c5b0827d5bdf63eadc9eb2e1136f6c494e90
qunshiyan1 <2420980652@qq.com>
2025-09-12 10:43:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ if weight[-2:] in ['kg']:
    bang = (eval (weight[0:-2]))/2.2046
    print("对应的英制重量为{:.3f}磅".format(bang))
 elif weight[-2:] in ['pd']:
-   qianke = (eval (weight[0:-2]))*2.2046
+   qianke = (eval (weight[0:-2]))*0.4535
    print("对应的公镑重量为{:.3f}公斤".format(qianke))
 else:
    print("输入格式错误")
